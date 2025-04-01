604411812f6baa638950aa759e4e22252918ca6c
victornext <victorbrs517@gmail.com>
2025-03-31 22:21:27 -0300

    SecurityFilter

--- stock/src/main/java/victornext/stock/Repositories/UserRepository.java
+++ stock/src/main/java/victornext/stock/Repositories/UserRepository.java
@@ -5,5 +5,8 @@ import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import victornext.stock.Model.UserModel;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<UserModel, String> {
+    Optional<UserModel> findByEmail(String email);
 }
