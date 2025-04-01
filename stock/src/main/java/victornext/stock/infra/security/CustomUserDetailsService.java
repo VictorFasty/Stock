161d4d4e5e349cfc6c296d09dc72dbea58f77c67
victornext <victorbrs517@gmail.com>
2025-03-31 22:28:09 -0300

    CustomUserDetailsService

---
+++ stock/src/main/java/victornext/stock/infra/security/CustomUserDetailsService.java
@@ -0,0 +1,22 @@
+package victornext.stock.infra.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import victornext.stock.Model.UserModel;
+import victornext.stock.Repositories.UserRepository;
+
+import java.util.ArrayList;
+
+public class CustomUserDetailsService implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserModel user = this.userRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException("User not found!!")) ;
+        return new org.springframework.security.core.userdetails.User(user.getEmail(), user.getPassword(), new ArrayList<>());
+    }
+}
