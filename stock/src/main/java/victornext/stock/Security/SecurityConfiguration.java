057b77e6e39be27c46c406fa83883b81cd143448
victornext <victorbrs517@gmail.com>
2025-05-02 00:04:22 -0300

    Resolve bug in the Security

--- stock/src/main/java/victornext/stock/Security/SecurityConfiguration.java
+++ stock/src/main/java/victornext/stock/Security/SecurityConfiguration.java
@@ -2,16 +2,11 @@ package victornext.stock.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,36 +14,22 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfiguration {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http)  throws Exception{
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .formLogin(
-                        configurer -> configurer.loginPage("/login").permitAll()
-                )
+                .formLogin(configurer -> configurer.loginPage("/login").permitAll())
                 .authorizeHttpRequests(authorize -> {
                     authorize.requestMatchers("/product/delete/**").hasRole("ADMIN");
                     authorize.requestMatchers("/Enterprises/delete/**").hasRole("ADMIN");
                     authorize.requestMatchers("/product/**").hasAnyRole("USER", "ADMIN");
                     authorize.requestMatchers("/Enterprises/**").hasAnyRole("USER", "ADMIN");
-                    {authorize.anyRequest().authenticated();}}
-                )
+                    authorize.anyRequest().authenticated();
+                })
                 .build();
     }
 
-
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder(10);
-    }
-
-
-    @Bean
-    public UserDetailsService userDetailsService(){
-        UserDetails user1 = User.builder()
-                .username("user")
-                .password("123")
-                .roles("USER")
-                .build();
-        return new InMemoryUserDetailsManager();
     }
 }
