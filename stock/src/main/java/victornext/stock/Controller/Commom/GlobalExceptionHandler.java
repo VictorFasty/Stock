0224cffabedbd14a816d0d216aff998c483e3d16
victornext <victorbrs517@gmail.com>
2025-05-08 16:13:47 -0300

    AcessDeniedException

--- stock/src/main/java/victornext/stock/Controller/Commom/GlobalExceptionHandler.java
+++ stock/src/main/java/victornext/stock/Controller/Commom/GlobalExceptionHandler.java
@@ -4,10 +4,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import victornext.stock.Exceptions.AcessDenied;
 import victornext.stock.Exceptions.DuplicatedException;
 import victornext.stock.Exceptions.InvalidField;
 import victornext.stock.Exceptions.NotFoundException;
@@ -104,6 +107,20 @@ public class GlobalExceptionHandler {
         return new ResponseEntity<>(body, HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(AccessDeniedException.class)
+    @ResponseStatus(HttpStatus.FORBIDDEN)
+    public ResponseEntity<?> handleAccessDenied(AccessDeniedException accessDeniedException) {
+        log.error("Access denied: {}", accessDeniedException.getMessage());
+
+        Map<String, Object> body = new HashMap<>();
+        body.put("timestamp", LocalDateTime.now());
+        body.put("status", HttpStatus.FORBIDDEN.value()); 
+        body.put("error", "Access Denied");
+        body.put("message", "You do not have permission to access this resource.");
+        body.put("details", accessDeniedException.getMessage());
+
+        return new ResponseEntity<>(body, HttpStatus.FORBIDDEN);
+    }
 
 
 }
---
+++ stock/src/main/java/victornext/stock/Exceptions/AcessDenied.java
@@ -0,0 +1,7 @@
+package victornext.stock.Exceptions;
+
+public class AcessDenied extends RuntimeException {
+    public AcessDenied(String message) {
+        super(message);
+    }
+}
