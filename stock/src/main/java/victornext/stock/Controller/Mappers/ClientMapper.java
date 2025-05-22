856f61fdb63f896f06b75e7389faaaebb5202e17
victornext <victorbrs517@gmail.com>
2025-05-22 15:17:51 -0300

    Not null in the ClientId Secret

--- stock/src/main/java/victornext/stock/Controller/Mappers/ClientMapper.java
+++ stock/src/main/java/victornext/stock/Controller/Mappers/ClientMapper.java
@@ -1,6 +1,7 @@
 package victornext.stock.Controller.Mappers;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import victornext.stock.Controller.DTOS.ClientDTO;
 import victornext.stock.Controller.DTOS.EnterprisesDTO;
 import victornext.stock.Model.ClientModel;
@@ -8,6 +9,11 @@ import victornext.stock.Model.EnterprisesModel;
 
 @Mapper(componentModel = "spring")
 public interface ClientMapper {
+
+    @Mapping(source = "clientId", target = "clientId")
+    @Mapping(source = "clientSecret", target = "clientSecret")
+    @Mapping(source = "redirectURI", target = "redirectURI")
+    @Mapping(source = "scope", target = "scope")
     ClientModel toEntity(ClientDTO dto);
 
     ClientDTO toDTO(ClientModel model);
